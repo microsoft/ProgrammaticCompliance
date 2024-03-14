9c4c2f84a196e314474c9c03c12f75a6e4a49317
Joel Otoude <joelotoude@microsoft.com>
2024-03-14 14:02:54 -0400

    Simplifying the environment config file and providing the links to install some of the modules

--- CustomPolicies/PowerShell/EnvConfig.ps1
+++ CustomPolicies/PowerShell/EnvConfig.ps1
@@ -1,6 +1,8 @@
 Write-Host "===== ENVIRONMENT CONFIGURATION ====="
 $CurrentErrorActionPreference = $ErrorActionPreference
 
+Set-ExecutionPolicy Bypass -Scope Process -Force
+
 $ErrorActionPreference = 'Stop'
 try {
     
@@ -8,8 +10,7 @@ try {
     Write-Host "Chocolatey is installed... Continuing"
 }
 catch {
-    Write-Host "Chocolatey is not installed. Installing it now..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force
+    Write-Host "Chocolatey is not installed. Installing it now..." 
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
@@ -22,18 +23,6 @@ catch {
     choco install armclient
 }
 
-if (Get-Module -Name Az -ListAvailable) {
-    Write-Host "The module Az is already installed. Continuing..."
-    Write-Host "Updating module Az to the latest version..."
-    Update-Module -Name Az -Force
-}
-else {
-    Write-Host "The module Az is not installed. Installing now..."
-    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-    Install-Module -Name Az -Repository PSGallery -Force
-}
-
-
 if (-not (Get-Module -Name Microsoft.PowerShell.PSResourceGet -ListAvailable)) {
     Write-Host "PSResourceGet not found. Please install PowerShell 7 and rerun the script"
     exit 1
@@ -41,8 +30,6 @@ if (-not (Get-Module -Name Microsoft.PowerShell.PSResourceGet -ListAvailable)) {
 
 if (Get-Module -Name Az.ResourceGraph -ListAvailable) {
     Write-Host "Az.ResourceGaph is already installed. Continuing..."
-    Write-Host "Updating the module Az.Resource to the latest version..."
-    Update-Module -Name Az.ResourceGraph -Force
 }
 else {
     Write-Host "The module Az.ResourceGraph is not installed. Installing now..."
