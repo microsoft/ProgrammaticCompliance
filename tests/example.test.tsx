275bab49a828af55949c81492116eb3a71f2c1a4
Andi Schabus <aschabus@microsoft.com>
2024-10-14 15:56:04 +0200

    Remove example test file

--- tests/example.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-
-test('renders learn react link', () => {
-  render(<a href="https://reactjs.org" > Learn React </a>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
